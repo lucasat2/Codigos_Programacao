e93658a28fbe26848396411681b0d3e0ecc3bd53
Lucas Ataide <41450845+lucasat2@users.noreply.github.com>
2024-06-25 13:39:22 -0300

    Datas em JS

---
+++ Cubos Academy/Unidade 2/8-Datas em JS/1-date.js
@@ -0,0 +1,12 @@
+// const date = new Date(2024,5,24,12,30,45,916);
+// console.log(date)
+
+// FINAL COPA DO MUNDO 2002
+
+const date2 = new Date(2002,5,30,8);
+console.log(date2)
+
+
+// Os horários são baseados no meridiano de greenwitch em londres, portanto, se nao for infomrada a hora, ele segue a de lá.
+//o padrao é : ANO,MES,DIA ,HORA,MINUTOS,SEGUNDOS,MILISEGUNDOS.
+//A contagem dos meses inicia em 0(janeiro), já a contagem dos dias começa em 1 normalmente.
